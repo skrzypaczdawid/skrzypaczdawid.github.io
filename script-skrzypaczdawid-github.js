56dd9b9e8598982b95c9f4e46ced19f7cc14e273
Dawid <skrzypaczdawid@gmail.com>
2022-01-25 13:12:40 +0100

    Created home page

---
+++ script-skrzypaczdawid-github.js
@@ -0,0 +1,20 @@
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+const three = document.querySelector('.three');
+const four = document.querySelector('.four');
+
+one.addEventListener('click', () => {
+    document.location.href = './Project1 - extending cards/index.html'
+})
+
+two.addEventListener('click', () => {
+    document.location.href = './Project2 - fluid counting slider/index.html'
+})
+
+three.addEventListener('click', () => {
+    document.location.href = './Project3 - hidden search bar/index.html'
+})
+
+four.addEventListener('click', () => {
+    document.location.href = './Project4 - loading site with blur background/index.html'
+})
