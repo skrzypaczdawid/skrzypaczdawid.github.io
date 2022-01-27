b258342dcf5fec311d8de30f56633e5b449c2e95
Dawid <skrzypaczdawid@gmail.com>
2022-01-27 13:27:06 +0100

    Fully working searchBar - new features

--- Project3 - hidden search bar/script.js
+++ Project3 - hidden search bar/script.js
@@ -1,16 +1,46 @@
-const button = document.getElementById('search')
-const text= document.getElementById('text')
-const input = document.querySelector('.search-box')
-const pvalue = document.getElementById('search-value')
+const button = document.getElementById('search') // Button with maginifying glass icon
+const text= document.getElementById('text') // input form which would slide on clik
+const input = document.querySelector('.search-box') // div which is holding input form inside
+const pvalue = document.getElementById('search-value') // place where text would be written to
+isActive = false
 
-button.addEventListener('click', () => {
-    input.classList.add('active')
+
+// Listener to activate or deactivate searchBar
+button.addEventListener('click', function(){
+    if(isActive)
+    {
+        deactivateSearch()
+    }
+    else{
+        activateSearch()
+    }
 })
 
-document.addEventListener("keyup", function(event) {
+// Function to add funcionality: after pressing Enter key the word would be written on the page
+ document.addEventListener("keyup", function(event) {
     if (event.code === 'Enter') {
-        pvalue.innerHTML = "Test your search bar: " + text.value
+        pvalue.innerHTML = "This is what you searched: " + text.value
         text.value = ''
         input.classList.remove('active')
+        isActive = false
     }
-});
+})
+
+// Activation of searchBar
+function activateSearch() {
+    input.classList.add('active')
+    isActive = true
+}
+
+// Dectivation of searchBar and also writting text from searchbar onto the page if text is not empty
+function deactivateSearch(){
+    if(text.value == '')
+    {
+        alert('Please enter something into search bar')
+        return
+    }
+    pvalue.innerHTML = "This is what you searched: " + text.value
+    text.value = ''
+    input.classList.remove('active')
+    isActive = false
+}
